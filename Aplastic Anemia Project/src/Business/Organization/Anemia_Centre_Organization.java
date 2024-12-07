fbc975c67ec7b6b60bd9bc6a02b03b59bd500ebe
saiteja1274 <lankapothu.s@northeastern.edu>
2024-12-07 13:08:24 -0500

    Creating Organization  Anemia_Centre_Organization

---
+++ Aplastic Anemia Project/src/Business/Organization/Anemia_Centre_Organization.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author saiteja
+ */
+public class Anemia_Centre_Organization extends Organization {
+     public Anemia_Centre_Organization() {
+        super(Organization.BloodCancerCentreType.BloodCancerCentre.getValue());
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new Anemia_Centre_Coordinator_Role());
+        return roles;
+    }
+    
+}
