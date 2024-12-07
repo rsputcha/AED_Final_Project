b16a9375821452be08fe82f2f0bedb6bada73e50
saiteja1274 <lankapothu.s@northeastern.edu>
2024-12-07 12:46:11 -0500

    uploading Bone_Marrow enterprise

---
+++ Aplastic Anemia Project/src/Business/Enterprise/Bone_Marrow_Bank_Enterprise.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+import java.util.ArrayList;
+import javax.management.relation.Role;
+
+/**
+ *
+ * @author saiteja
+ */
+public class Bone_Marrow_Bank_Enterprise {
+    public Bone_Marrow_Bank_Enterprise(String name){
+        super(name,EnterpriseType.BoneMarrowBank);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
