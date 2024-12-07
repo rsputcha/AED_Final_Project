0ac0d9d54e44c5d9588022b5de40257977e88231
saiteja1274 <lankapothu.s@northeastern.edu>
2024-12-07 12:30:13 -0500

    uploading business

---
+++ Aplastic Anemia Project/src/Business/ConfigureASystem.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+/**
+ *
+ * @author saiteja
+ */
+public class ConfigureASystem {
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
+    
+}
